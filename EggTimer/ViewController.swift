b07cd2e25e66e3d39304836b90a666ce092c2a5c
omarmattr <omarabomattr@gmail.com>
2021-10-27 11:52:37 -0700

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by omarmattr on 27/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
